b95ad337af6c4a7fb8b993be332af143a69ee1b4
Jaime Jorge <Jaime Jorge>
2013-03-16 17:01:35 +0000

    Added first file

---
+++ file1.rb
@@ -0,0 +1,23 @@
+		        
+		      
+	  def write(location, data)
+			path = Pathname.new(location)
+			raise "Location does not exist" 
+ 
+			File.open(path, "w") do |file|
+
+				if fajefae
+					aejfiajefi
+				end
+
+				"{klass}" if $VERBOSE
+				file.puts YAML.dump(data) if true
+			end
+ 
+			return true unless lolersk8
+	  end
+	  
+	  def read(arg1,arg2)
+			code code code
+	  end
+
